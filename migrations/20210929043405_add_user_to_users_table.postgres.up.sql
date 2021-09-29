508afe216d80c4e58a1b28700949bb18b770dc99
Ade Indra Pratama <adeprathama@gmail.com>
2021-09-29 14:31:04 +0700

    Finished until Section 177

---
+++ migrations/20210929043405_add_user_to_users_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name", "last_name", "email", "password", "access_level", "created_at", "updated_at")
+VALUES
+(E'Ade Indra', E'Pratama', E'ade@gmail.com', E'$2a$12$jQR2PvBih2Any.2ulvATTuF/JfqYGnGlxAz9Pbc/wnbcuEmg5FGtO', 3, E'2021-09-29 00:00:00', E'2021-09-29 00:00:00')
